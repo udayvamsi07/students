3ee3a8214a557eb388733ae08927ac66191a8c8c
uday vamsi <udayvamsi07@gmail.com>
2025-11-12 18:52:02 -0600

    first commit

---
+++ src/main/java/com/marks/App.java
@@ -0,0 +1,38 @@
+package com.marks;
+
+import java.sql.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        1)import the package
+        2)load and register the driver
+        3)esablish the connection
+        4)create the statement
+        5)execute the query or update
+        6)process the results   
+        7)close the connection
+         */
+
+        String url="jdbc:postgresql://Localhost:5432/studentmarks";
+        String user="postgres";
+        String password="1234";
+        String sql="Select name from marks where sid=1";
+       Class.forName("org.postgresql.Driver");
+      
+      Connection con=DriverManager.getConnection(url,user,password);
+        System.out.println("connection established");
+        Statement st=con.createStatement();
+        ResultSet rs=st.executeQuery(sql);
+       rs.next();
+        String res=rs.getString("name");
+        
+    System.out.println("name is :"+res);
+            con.close();
+        System.out.println("connection closed");
+        
+
+
+        
+    }
+    
+}
